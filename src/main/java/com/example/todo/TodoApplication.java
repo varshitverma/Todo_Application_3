8bda110ae222fca04d15e68c42dbb7ecdd180384
Varshit Verma <pvarshitv@gmail.com>
2023-03-21 20:24:17 +0530

    Update TodoApplication.java

--- src/main/java/com/example/todo/TodoApplication.java
+++ src/main/java/com/example/todo/TodoApplication.java
@@ -4,8 +4,8 @@ package com.example.todo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public  class TodoApplication {
+@SpringBootApplication(scanBasePackages = {"com.example.todo.controller", "com.example.todo.service"})
+public class TodoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
 	}
